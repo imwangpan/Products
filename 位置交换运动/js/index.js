6297355e6e87b0b0b24e1d7d20b82609c4a88424
imwangpan <imwangpan@gmail.com>
2018-05-22 22:17:40 +0800

    位置交换运动

--- 位置交换运动/js/index.js
+++ 位置交换运动/js/index.js
@@ -1,33 +1,58 @@
-window.onload=function(){
-	var ups=document.querySelectorAll("button.up")
-	var downs=document.querySelectorAll("button.down")
-	var lis=document.querySelectorAll("li")		//静态获取
-	var lisD=document.getElementsByTagName("li")		//静态获取
-	var ul=document.querySelector("ul")
-	console.log(lisD)
-	
-	//上移
-	for(i=0;i<ups.length;i++){
-		//ups[i].index=i
-		ups[i].onclick=function(){
-			if(this==lisD[0].children[1]){		//第一个 li 的第二个子元素，也就是“上移”按钮
-				alert("走不动了")
-			}else{
-				ul.insertBefore(this.parentNode,this.parentNode.previousElementSibling)
-			}
+var lis=document.querySelectorAll("li")		//静态获取
+var ul=document.querySelector("ul")
+var h=lis[0].offsetHeight+parseFloat(getComputedStyle(lis[0]).marginTop)		//要运动的距离，包括 li 的高和 margin-top 值
+console.log(lis)
+
+for(var i=0;i<lis.length;i++){
+	var up=lis[i].querySelector("button.up")
+	var down=lis[i].querySelector("button.down")
+	console.log(up,down)
+	//上移功能
+	up.onclick=function(){
+		//在这里需要找到当前所在的 li 与上一个 li
+		var cur=this.parentNode;		//当前点击按钮的父级，也就是当前点击按钮所在的 li
+		var prev=cur.previousElementSibling;		//当前所在 li 的上一个 li
+		console.log(cur,prev)
+		
+		if(!prev){		//如果当前所在 li 的上一个 li 不存在，说明已经到头了
+			alert('到头了');
+			return;
 		}
-	}
-	
-	//下移
-	for(i=0;i<downs.length;i++){
-		downs[i].onclick=function(){
-			if(this==lisD[3].children[2]){		//第四个 li 的第三个子元素，也就是“下移”按钮
-				alert("走不动了")
-			}else{
-				ul.insertBefore(this.parentNode.nextElementSibling,this.parentNode)
-			}
+		
+		/*
+		 * 1、结构里的顺序没有变
+		 * 2、相对定位造成位置不对
+		 */
+		move(cur,{top:-h},200,'linear',end);
+		move(prev,{top:h},200,'linear',end);
+		
+		function end(){
+			ul.insertBefore(cur,prev);
+			cur.style.top=prev.style.top=0;
 		}
-	}
+	};
 	
-	
+	//下移功能
+	down.onclick=function(){
+		//在这里需要找到当前所在的 li 与下一个 li
+		var cur=this.parentNode;		//当前点击按钮的父级，也就是当前点击按钮所在的 li
+		var next=cur.nextElementSibling;		//当前所在 li 的下一个 li
+		
+		if(!next){		//如果当前所在 li 的下一个 li 不存在，说明已经到头了
+			alert('到头了');
+			return;
+		}
+		
+		/*
+		 * 1、结构里的顺序没有变
+		 * 2、相对定位造成位置不对
+		 */
+		move(cur,{top:h},200,'linear',end);
+		move(next,{top:-h},200,'linear',end);
+		
+		function end(){
+			ul.insertBefore(next,cur);
+			cur.style.top=next.style.top=0;
+		}
+	};
 }
