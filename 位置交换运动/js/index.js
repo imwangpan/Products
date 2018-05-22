b68f4e747b750dfd65956cd1408a9450fccb79ef
imwangpan <imwangpan@gmail.com>
2018-05-22 22:32:53 +0800

    位置交换运动

--- 位置交换运动/js/index.js
+++ 位置交换运动/js/index.js
@@ -20,15 +20,15 @@ for(var i=0;i<lis.length;i++){
 		}
 		
 		/*
-		 * 1、结构里的顺序没有变
-		 * 2、相对定位造成位置不对
+		 * li 采用的是相对定位，初始定位置（top）为 0，这里的运动只是动画效果，实际上位置的改变是通过 DOM 方法改变了结构中的标签顺序。
+		 * 例如：蓝色上移，是把蓝色 li 标签插入到红色 li 标签的前面，这里改变蓝色 li 和红色 li 的定位值只是为了运动的动画效果，当运动完成后，由于它们位置的改变是由于结构中标签位置发生了变化（并不是定位置的改变），所以需要将它们的定位置都修正为初始值 0。
 		 */
 		move(cur,{top:-h},200,'linear',end);
-		move(prev,{top:h},200,'linear',end);
+		move(prev,{top:h},200,'linear',end);		//这里的位置改变只是为了运动的动画效果
 		
 		function end(){
-			ul.insertBefore(cur,prev);
-			cur.style.top=prev.style.top=0;
+			ul.insertBefore(cur,prev);		//将当前所在 li 插入到上一个 li 的前面。这才是位置改变的真正原因
+			cur.style.top=prev.style.top=0;		//修正它们的定位置
 		}
 	};
 	
