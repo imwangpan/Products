56e80d2250e81d6dc4b5cb1c643b0cb4f8fdc893
imwangpan <imwangpan@gmail.com>
2018-05-23 22:42:11 +0800

    键鼠控制方块移动

--- 键鼠控制方块移动/js/index.js
+++ 键鼠控制方块移动/js/index.js
@@ -1,4 +1,5 @@
 var box=document.getElementById("box");
+//声明一个对象用来存放键盘上按键的键码 keyCode 以及其对应的布尔值。初始时这个对象里可以不放数据
 var key={
 	//上下左右箭头
 	38:false,
@@ -12,16 +13,16 @@ var key={
 	68:false
 };
 document.onkeydown=function(ev){
-	key[ev.keyCode]=true;
+	key[ev.keyCode]=true;		//在发送键盘按键按下事件时，将这个按键的 keyCode 作为属性，true 作为属性值添加到对象 key 中（如果相应属性已经存在就是更新属性值）。
 	//console.log(key);
 };
 document.onkeyup=function(ev){
-	key[ev.keyCode]=false;
+	key[ev.keyCode]=false;		//在发送键盘按键抬起事件时，更新这个按键的 keyCode 对应的属性值为 false。
 	//console.log(key);
 };
 
 setInterval(function(){
-	if(key[37]||key[65]){
+	if(key[37]||key[65]){		//当对象中 37（或 65）对应的属性值为 true 时，表示按下了左箭头（A 键）
 		box.style.left=box.offsetLeft-5+'px';
 	}
 	
