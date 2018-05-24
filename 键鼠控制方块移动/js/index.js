001a23b6d3f3fd4ea429c6c072d7e06584d49fae
imwangpan <imwangpan@gmail.com>
2018-05-24 21:26:32 +0800

    键鼠控制方块移动

--- 键鼠控制方块移动/js/index.js
+++ 键鼠控制方块移动/js/index.js
@@ -1,4 +1,5 @@
 var box=document.getElementById("box");
+
 //声明一个对象用来存放键盘上按键的键码 keyCode 以及其对应的布尔值。初始时这个对象里可以不放数据
 var key={
 	//上下左右箭头
@@ -12,6 +13,17 @@ var key={
 	83:false,
 	68:false
 };
+var maxHeight=window.innerHeight-box.offsetHeight;
+var maxWidth=window.innerWidth-box.offsetWidth;
+var L=0,T=0;
+console.log(maxHeight,maxWidth)
+window.onresize=function(){
+	//窗口大小变化后，重新获取一下值
+	maxHeight=window.innerHeight-box.offsetHeight;
+	maxWidth=window.innerWidth-box.offsetWidth;
+	//console.log(maxHeight,maxWidth)
+}
+
 document.onkeydown=function(ev){
 	key[ev.keyCode]=true;		//在发送键盘按键按下事件时，将这个按键的 keyCode 作为属性，true 作为属性值添加到对象 key 中（如果相应属性已经存在就是更新属性值）。
 	//console.log(key);
@@ -21,20 +33,78 @@ document.onkeyup=function(ev){
 	//console.log(key);
 };
 
+//键盘控制移动
+//通过定时器每隔一段时间调用一次，使方块平滑移动
 setInterval(function(){
 	if(key[37]||key[65]){		//当对象中 37（或 65）对应的属性值为 true 时，表示按下了左箭头（A 键）
-		box.style.left=box.offsetLeft-5+'px';
+		//直接通过条件语句控制范围
+		/*L=box.offsetLeft-5
+		if(L<0){
+			L=0
+		}*/
+		//三目运算符
+		//L=box.offsetLeft-5<0?0:box.offsetLeft-5
+		
+		//通过函数控制范围
+		L=limits(box.offsetLeft-5,'left')
+		box.style.left=L+'px';
 	}
 	
 	if(key[38]||key[87]){
-		box.style.top=box.offsetTop-5+'px';
+		T=box.offsetTop-5<0?0:box.offsetTop-5
+		box.style.top=T+'px';
 	}
 	
 	if(key[39]||key[68]){
-		box.style.left=box.offsetLeft+5+'px';
+		L=box.offsetLeft+5>maxWidth?maxWidth:box.offsetLeft+5
+		box.style.left=L+'px';
 	}
 	
 	if(key[40]||key[83]){
-		box.style.top=box.offsetTop+5+'px';
+		//T=box.offsetTop+5>maxHeight?maxHeight:box.offsetTop+5
+		T=limits(box.offsetTop+5,'top')
+		box.style.top=T+'px';
 	}
 },16);
+
+//鼠标拖拽
+box.onmousedown=function(event){
+	var X=event.pageX-box.offsetLeft
+	var Y=event.pageY-box.offsetTop
+	document.onmousemove=function(event){
+		L=limits(event.pageX-X,'left')
+		T=limits(event.pageY-Y,'top')
+		box.style.left=L+'px'
+		box.style.top=T+'px'
+	}
+}
+box.onmouseup=function(){
+	document.onmousemove=null;
+}
+
+//双击变色
+box.ondblclick=function(){
+	box.style.background=box.style.background=='deepskyblue'?'#DDDDDD':'deepskyblue'
+}
+
+//限制移动范围
+function limits(n,str){
+	switch (str){
+		case 'left':
+			if(n<0){
+				n=0
+			}else if(n>maxWidth){
+				n=maxWidth
+			}
+			return n
+			/*break*/		//因为已经有 return 了，所以此处 break 可以省略，不影响结果
+		case 'top':
+			if(n<0){
+				n=0
+			}else if(n>maxHeight){
+				n=maxHeight
+			}
+			return n
+			/*break*/
+	}
+}
