b327773b1d805466b08530e66dd87e5d0ee39f36
imwangpan <imwangpan@gmail.com>
2018-05-25 21:29:48 +0800

    键盘控制图片翻页

--- 键盘控制图片翻页/js/index.js
+++ 键盘控制图片翻页/js/index.js
@@ -1,76 +1,54 @@
-var imgX=document.querySelector("#imgX")
-var imgY=document.querySelector("#imgY")
-var L=-300
-var T=-300
-document.onkeydown=function(ev){
-	switch(ev.keyCode){
-		//左箭头：37		上箭头：38		右箭头：39		下箭头：40
-		case 37:
-			moveto("left")
-			break;
-		case 38:
-			moveto("top")
-			break;
-		case 39:
-			moveto("right")
-			break;
-		case 40:
-			moveto("bottom")
-			break;
-	}
-}
+var box=document.querySelector("#img")
+var lis=box.getElementsByTagName('li')
+var w=box.offsetWidth
+var moving=true
 
-//运动
-function moveto(direction){
-	if(direction=="top"){
-		T-=300
-		move(imgX,{top:-300},500,"linear")
-		move(imgY,{top:T},500,"linear",function(){
-			imgX.style.top=0
-			imgX.style.zIndex=1
-			imgY.style["z-index"]=2		//两种写法
-			//console.log(T)
-			if(T==-2100){
-				imgY.style.top=-600+"px"
-				T=-600
-			}
-		})
-	}else if(direction=="right"){
-		L+=300
-		move(imgY,{left:300},500,"linear")
-		move(imgX,{left:L},500,"linear",function(){
-			imgY.style.left=0
-			imgX.style["z-index"]=2
-			imgY.style["z-index"]=1
-			if(L==0){
-				imgX.style.left=-1500+"px"
-				L=-1500
-			}
-		})
-	}else if(direction=="bottom"){
-		T+=300
-		move(imgX,{top:300},500,"linear")
-		move(imgY,{top:T},500,"linear",function(){
-			imgX.style.top=0
-			imgX.style.zIndex=1
-			imgY.style["z-index"]=2
-			//console.log(T)
-			if(T==0){
-				imgY.style.top=-1800+"px"
-				T=-1800
-			}
-		})
-	}else if(direction=="left"){
-		L-=300
-		move(imgY,{left:-300},500,"linear")
-		move(imgX,{left:L},500,"linear",function(){
-			imgY.style.left=0
-			imgX.style["z-index"]=2
-			imgY.style["z-index"]=1
-			if(L==-1800){
-				imgX.style.left=-300+"px"
-				L=-300
-			}
-		})
+document.onkeydown=function(ev){
+	if(!moving){
+		return
+	}
+	moving=false
+	switch(ev.keyCode){
+		//左箭头：37	上箭头：38	右箭头：39	下箭头：40
+		//A：65		W：87		D：68		S：65
+		case 37||65:	//左
+		console.log(box,w)
+			box.style.width=w*lis.length+'px';		//先让图片横着排
+			move(box,{left:-w},400,'linear',function(){
+				box.appendChild(lis[0]);
+				this.style.left=0;
+				
+				moving=true;
+			});
+			break;
+			
+		case 38||87:	//上
+			box.style.width=w+'px';		//先让图片竖着排
+			move(box,{top:-w},400,'linear',function(){
+				box.appendChild(lis[0]);
+				this.style.top=0;
+				
+				moving=true;
+			});
+			break;
+			
+		case 39||68:	//右
+			box.style.width=w*lis.length+'px';		//先让图片横着排
+			//一上来的时候，需要把最后一张图放到最前面，才能无缝
+			box.insertBefore(lis[lis.length-1],lis[0]);
+			box.style.left=-w+'px';	//不要让最后一张图一下来就显示出来
+			move(box,{left:0},400,'linear',function(){
+				moving=true;
+			});
+			break;
+			
+		case 40||65:	//下
+			box.style.width=w+'px';		//先让图片横着排
+			//一上来的时候，需要把最后一张图放到最前面，才能无缝
+			box.insertBefore(lis[lis.length-1],lis[0]);
+			box.style.top=-w+'px';	//不要让最后一张图一下来就显示出来
+			move(box,{top:0},400,'linear',function(){
+				moving=true;
+			});
 	}
 }
